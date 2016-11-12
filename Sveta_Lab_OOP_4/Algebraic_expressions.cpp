a5b5f4d177955c8b37657b7b34a2fe12f8384b24
Zhenia Trochun <zheniatrochun@ukr.net>
2016-11-12 14:25:10 +0200

    add finding integral

--- Sveta_Lab_OOP_4/Algebraic_expressions.cpp
+++ Sveta_Lab_OOP_4/Algebraic_expressions.cpp
@@ -49,8 +49,13 @@ Algebraic_expressions Algebraic_expressions::operator!()
 {
 	string *parseResults;
 	string integral;
+	int counter = 0;
 
-	Algebraic_expressions ae;
+	parseResults = parseStr(&counter);
+
+	integral = findIntegral(parseResults, counter);
+
+	Algebraic_expressions ae(integral);
 
 	return ae;
 }
@@ -102,6 +107,8 @@ string* Algebraic_expressions::parseStr(int *counter)
 
 string Algebraic_expressions::findIntegral(string* str, int counter)
 {
+	string result;
+	string *resArr = new string[counter]; // arr jf resulting strings
 	string *expr = new string[10]; // arr for expressions
 	string *integr = new string[10]; // arr for integrals of expressions
 	expr[0] = "exp(x)";
@@ -118,15 +125,128 @@ string Algebraic_expressions::findIntegral(string* str, int counter)
 	integr[0] = "exp(x)";
 	integr[1] = "ln(x)";
 	integr[2] = "sin(x)";
-	integr[3] = "-cos(x)";
+	integr[3] = "- cos(x)";
 	integr[4] = "tg(x)";
-	integr[5] = "-ctg(x)";
+	integr[5] = "- ctg(x)";
 	integr[6] = "arcsin(x)";
 	integr[7] = "arctg(x)";
 	integr[8] = "ln|(1-cos(x))/(sin(x))|";
 	integr[9] = "ln|(1+sin(x))/(cos(x))|";
 	// ...
 
+	for (int i(0); i < counter; i++) 
+	{
+		if (str[i].at(0) == '-')
+		{
+			resArr[i] += "- ";
+			str[i] = str[i].substr(1, 1024);
+		}
+	}
+
+	for (int i(0); i < counter; i++)
+	{
+		if (str[i] == "") continue;
+		if (isdigit(str[i].at(0)))
+		{
+			bool check = true;
+			int index = 0;
+
+			for (int j(1); j < str[i].length(); j++)
+			{
+				if(!isdigit(str[i].at(j)) && str[i].at(j) != '.')
+				{
+					check = false;
+				}
+				if (str[i].at(j) == '*')
+				{
+					index = j;
+				}
+			}
+
+			if (check)
+			{
+				resArr[i] += str[i] + "*x";
+				str[i] = "";
+			}
+
+			if (index != 0)
+			{
+				resArr[i] += str[i].substr(0, index + 1);
+				str[i] = str[i].substr(index + 1, 1024);
+			}
+		}
+	}
+
+	for (int i(0); i < counter; i++)
+	{
+		int ind = str[i].find("x^(");
+		if (ind == 0)
+		{
+			string tmp = str[i].substr(3, str[i].length() - 4);
+			bool check = true;
+
+			for (int j(0); j < tmp.length(); j++)
+			{
+				if (!isdigit(tmp.at(j)))
+				{
+					check = false;
+					break;
+				}
+			}
+
+			if (!check)
+			{
+				resArr[i] += "";
+			}
+			
+			if (check)
+			{
+				const char *charArr = tmp.c_str();
+				char buf[1024];
+				int num = atoi(charArr);
+				num++;
+				resArr[i] += "(x^(";
+				resArr[i] += itoa(num, buf, 1024);
+				resArr[i] += "))/";
+				resArr[i] += itoa(num, buf, 1024);
+				str[i] = "";
+			}
+		}
+	}
+
+	for (int i(0); i < counter; i++)
+	{
+		for (int j(0); j < 10; j++)
+		{
+			if (str[i].compare(expr[j]) == 0)
+			{
+				resArr[i] += integr[j];
+				str[i] = "";
+			}
+		}
+	}
+
+	for (int i(0); i < counter; i++)
+	{
+		if (!str[i].empty())
+		{
+			resArr[i] += "integral_of(" + str[i] + ")";
+		}
+
+		if (i == 0)
+		{
+			result += resArr[i];
+		}
+		else
+		{
+			if (resArr[i].at(0) == '-')
+				result += " " + resArr[i];
+			else
+				result += " + " + resArr[i];
+		}
+	}
+
+	return result + " + C";
 }
 
 
--- Sveta_Lab_OOP_4/Sveta_Lab_OOP_4.cpp
+++ Sveta_Lab_OOP_4/Sveta_Lab_OOP_4.cpp
@@ -7,9 +7,9 @@
 
 int main()
 {
-	Algebraic_expressions e1("x^2 + 5 - 7 + x + x1 + x2");
+	Algebraic_expressions e1("x^(2) + 5");
 
-	Algebraic_expressions e2("3 * x^2 - 7");
+	Algebraic_expressions e2("7 + 2*cos(x) - 3*x^(2) - sin(x)");
 
 	Algebraic_expressions e3 = -e1;
 
@@ -17,16 +17,10 @@ int main()
 
 	Algebraic_expressions e5 = e1 - e2;
 
-	cout << e3.getExpression() << endl;
-	cout << e4.getExpression() << endl;
-	cout << e5.getExpression() << endl;
+	Algebraic_expressions e6 = !e2;
 
-	/*
-	int counter = 0;
-	string *parse = e1.parseStr(&counter);
-
-	for (int i(0); i < counter; i++)
-		cout << parse[i] << endl;*/
+	cout << e2.getExpression() << endl;
+	cout << e6.getExpression() << endl;
 
 	system("pause");
 
